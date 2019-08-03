64e735e942f2e18fc9835cc85d1d95de96bda8f7
jahnvi01 <jbdalwadi01@gmail.com>
2019-08-04 00:41:57 +0530

    heroku

--- server.js
+++ server.js
@@ -18,6 +18,13 @@ if(process.env.NODE_ENV==="production"){
         res.sendFile(path.resolve(__dirname,'client','build','index.html'));
     })
 }
+
+var server_port = process.env.YOUR_PORT || process.env.PORT || 80;
+var server_host = process.env.YOUR_HOST || '0.0.0.0';
+server.listen(server_port, server_host, function() {
+    console.log('Listening on port %d', server_port);
+});
+
 // app.listen(port);
-var server=app.listen(port,function() {
-    console.log(`app running on port${port}`); });
+// var server=app.listen(port,function() {
+//     console.log(`app running on port${port}`); });

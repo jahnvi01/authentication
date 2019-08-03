db930107d21613743fd06b9ffc346382dbafefcb
jahnvi01 <jbdalwadi01@gmail.com>
2019-08-04 00:47:12 +0530

    heroku

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ if(process.env.NODE_ENV==="production"){
 
 var server_port = process.env.YOUR_PORT || process.env.PORT || 80;
 var server_host = process.env.YOUR_HOST || '0.0.0.0';
-server.listen(server_port, server_host, function() {
+app.listen(server_port, server_host, function() {
     console.log('Listening on port %d', server_port);
 });
 

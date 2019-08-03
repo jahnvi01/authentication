f6b5ebbef50a72e5d34d44ad1a06f010c2ba4407
jahnvi01 <jbdalwadi01@gmail.com>
2019-08-03 23:02:56 +0530

    heroku

--- server.js
+++ server.js
@@ -6,12 +6,18 @@ const app=express();
 const info =require('./routes/api/info');
 const testRoute=require('./routes/api/questionRoute');
 const db=require('./db/db');
+const path=require('path');
 app.use(cors());
 app.use(bodyparser.json());
 app.use('/api/test',testRoute);
 app.use('/api/info',info);
 var port = process.env.port || 8080;
-
+if(process.env.NODE_ENV==="production"){
+    app.use(express.static('client/build'));
+    app.get('*',(req,res)=>{
+        res.sendFile(path.resolve(__dirname,'client','build','index.html'));
+    })
+}
 // app.listen(port);
 var server=app.listen(port,function() {
     console.log(`app running on port${port}`); });
